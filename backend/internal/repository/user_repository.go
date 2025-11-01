69cafd60d6f7b612a62f134f5e242d2e368c22e8
rikut0904 <rkt9tuba4robot@gmail.com>
2025-11-01 14:07:48 +0900

    fix/バックエンド構成の修正

---
+++ backend/internal/repository/user_repository.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+
+	"zerodelay/internal/domain/model"
+	"zerodelay/internal/domain/repository"
+)
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+// NewUserRepository creates a new user repository
+func NewUserRepository(db *gorm.DB) repository.UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Create(user *model.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByID(id uint) (*model.User, error) {
+	var user model.User
+	if err := r.db.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) FindAll() ([]model.User, error) {
+	var users []model.User
+	if err := r.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (r *userRepository) Update(user *model.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *userRepository) Delete(id uint) error {
+	return r.db.Delete(&model.User{}, id).Error
+}
