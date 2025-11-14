7df0eccd9bdf344a1f09fb525d5446ac82ef498e
rikut0904 <rkt9tuba4robot@gmail.com>
2025-11-14 13:52:26 +0900

    fix/エラーハンドリング

--- backend/internal/service/place_service.go
+++ backend/internal/service/place_service.go
@@ -1,9 +1,15 @@
 package service
 
 import (
+	"errors"
+
+	"gorm.io/gorm"
+
 	"zerodelay/internal/domain/model"
 	"zerodelay/internal/domain/repository"
 )
+
+var ErrPlaceNotFound = errors.New("place not found")
 
 // PlaceService handles business logic for places
 type PlaceService struct {
@@ -20,7 +26,14 @@ func (s *PlaceService) CreatePlace(place *model.Place) error {
 }
 
 func (s *PlaceService) GetPlace(id uint) (*model.Place, error) {
-	return s.placeRepo.FindByID(id)
+	place, err := s.placeRepo.FindByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrPlaceNotFound
+		}
+		return nil, err
+	}
+	return place, nil
 }
 
 func (s *PlaceService) GetAllPlaces() ([]model.Place, error) {
